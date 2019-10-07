07ab248c9f231c72cc4248bfea8d76158dabb88e
Luis Cintron <l.cintron@live.com>
2019-10-06 20:34:53 -0400

    added examply python code and server package.json for npm  packages

--- ui/example.py
+++ ui/example.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
+#----------------------------------------------------
+# File: example.py
+# Description: display status on e-Paper display
+#----------------------------------------------------
 import sys
 import os
 picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic/star-wars-icons')
@@ -18,70 +22,37 @@ import traceback
 logging.basicConfig(level=logging.DEBUG)
 
 try:
-	pic_list = os.listdir(picdir)
-	print(pic_list)
+    pic_list = os.listdir(picdir)
+    print(pic_list)
     logging.info("epd2in13_V2 Demo")
 
     epd = epd2in13_V2.EPD()
-    logging.info("init and Clear")
-    epd.init(epd.FULL_UPDATE)
-    epd.Clear(0xFF)
+#    logging.info("init and Clear")
+#    epd.init(epd.FULL_UPDATE)
+#    epd.Clear(0xFF)
 
-    # Drawing on the image
-    font15 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 15)
-    font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
+    font15 = ImageFont.truetype(os.path.join(picdir, '../Font.ttc'), 15)
 
-    logging.info("1.Drawing on the image...")
-    image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
-    draw = ImageDraw.Draw(image)
-
-    draw.rectangle([(0,0),(50,50)],outline = 0)
-    draw.rectangle([(55,0),(100,50)],fill = 0)
-    draw.line([(0,0),(50,50)], fill = 0,width = 1)
-    draw.line([(0,50),(50,0)], fill = 0,width = 1)
-    draw.chord((10, 60, 50, 100), 0, 360, fill = 0)
-    draw.ellipse((55, 60, 95, 100), outline = 0)
-    draw.pieslice((55, 60, 95, 100), 90, 180, outline = 0)
-    draw.pieslice((55, 60, 95, 100), 270, 360, fill = 0)
-    draw.polygon([(110,0),(110,50),(150,25)],outline = 0)
-    draw.polygon([(190,0),(190,50),(150,25)],fill = 0)
-    draw.text((120, 60), 'e-Paper demo', font = font15, fill = 0)
-    draw.text((110, 90), u'微雪电子', font = font24, fill = 0)
-    epd.display(epd.getbuffer(image))
-    time.sleep(2)
-
-    # read bmp file
-    #logging.info("2.read bmp file...")
-    #image = Image.open(os.path.join(picdir, 'google.bmp'))
+    #logging.info("0. quick test")
+    #image = Image.open(os.path.join(picdir, 'bb8.bmp'))
     #epd.display(epd.getbuffer(image))
-    #time.sleep(2)
+    #time.sleep(5)
 
-    # read bmp file on window
-    #logging.info("3.read bmp file on window...")
-    # epd.Clear(0xFF)
-    #image1 = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
-    #bmp = Image.open(os.path.join(picdir, '128.png'))
-    #image1.paste(bmp, (2,2))
-    #epd.display(epd.getbuffer(image1))
-    #time.sleep(4)
-
-    # # partial update
-    logging.info("2. show star wars...")
+# # partial update
+    logging.info("1. show star wars...")
     sw_image = Image.new('1', (epd.height, epd.width), 255)
-    #sw_draw = ImageDraw.Draw(sw_image)
-
+    sw_draw = ImageDraw.Draw(sw_image)
     epd.init(epd.FULL_UPDATE)
     epd.displayPartBaseImage(epd.getbuffer(sw_image))
 
     epd.init(epd.PART_UPDATE)
-    
     for file_ in pic_list:
-        #time_draw.rectangle((120, 80, 220, 105), fill = 255)
-        #time_draw.text((120, 80), time.strftime('%H:%M:%S'), font = font24, fill = 0)
-		bmp = Image.open(file_);
-		sw_image.paste(bmp, (2,2))
+        bmp = Image.open(os.path.join(picdir,file_))
+        sw_draw.rectangle((2,2,220,100), fill=255)
+        sw_draw.text((2,2), file_, font=font15, fill=0)
+        sw_image.paste(bmp, (20,20))
         epd.displayPartial(epd.getbuffer(sw_image))
-        time.sleep(2)
+        time.sleep(8)
 
     logging.info("Clear...")
     epd.init(epd.FULL_UPDATE)
